2ba660e0342b2cb03b85ae3c6174068e03218fcd
sezer okmen <szrokmn34@gmail.com>
2023-05-21 09:45:06 -0400

    node

--- 2-express.js/2-Nodemon/blogapp/index.js
+++ 2-express.js/2-Nodemon/blogapp/index.js
@@ -9,3 +9,14 @@ app.use(function(req, res) {
 app.listen(3000, function() {
     console.log("listening on port 3000");
 });
+
+
+
+// nodemon`a neden ihtiyac duyariz?
+// Degisikliklerin otomatik oalrak algilanmasi icin nodemon kurulumu yapilir.
+// npm i nodemon@2.0.19 --save-dev  (devDependicies kisminda nodemon kurulmus olur)
+// npm i nodemon@2.0.19 
+// devDependencies deki paketler projenin ihtiyac duydugu paketller degildir.Proje gelistirilirken bize yardimci olacak araclardir
+// bundan sonra nodemon diyerek calistiririz
+// node => bir komuttur 
+// nodemon => bir kutuphanedir (npx nodemon  =>  kutuphane oldugu icin npx dedik)
