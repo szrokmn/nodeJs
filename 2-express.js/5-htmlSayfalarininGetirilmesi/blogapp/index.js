9a9828f9990f4e45e0144efcab01466d6875e218
sezer okmen <szrokmn34@gmail.com>
2023-05-21 12:25:59 -0400

    node

--- 2-express.js/5-htmlSayfalarininGetirilmesi/blogapp/index.js
+++ 2-express.js/5-htmlSayfalarininGetirilmesi/blogapp/index.js
@@ -21,3 +21,12 @@ app.use("/", function(req, res) {
 app.listen(3000, function() {
     console.log("listening on port 3000");
 });
+
+
+// views klasoru icerisinde html dosyalarimiz olacak
+// sendFile() dosya geri dondurur
+// global degisken => __dirname => suan da bulundugumuz klasor ismini verir
+// global degisken => __filename => suan da bulundugumuz dosya ismini verir
+// path kutuphanesi var ve kullanabilmek icin  tanimlamak gerek (const path = require("path"))
+// path ile yollari birlestiriyoruz
+// proje yolu ile views yolunu birlestirecegiz
